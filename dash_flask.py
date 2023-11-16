89a3e991f3b769177bc45eb9858a3fc87399a437
Guillaume-star <69424028+Guillaume-star@users.noreply.github.com>
2023-11-16 18:33:30 -0500

    Update dash_flask.py

--- dash_flask.py
+++ dash_flask.py
@@ -1,46 +1,25 @@
-from dash.dependencies import Output, Input, State
-import dash_bootstrap_components as dbc
-from dash import dcc
-from dash import html
+from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
-from flask import Flask
 import pandas as pd
-import dash
 
-server = Flask(__name__)
-app = dash.Dash(server=server, external_stylesheets=[dbc.themes.CYBORG])
-app.title = 'Dashboard'
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
+app = Dash(__name__)
+server = app.server
 
-app.layout = dbc.Container([ 
-
- dbc.Row(dbc.Col(html.H2("Your Amazing Dashboard"), width={'size': 12, 'offset': 0, 'order': 0}), style = {'textAlign': 'center', 'paddingBottom': '1%'}),
-
- dbc.Row(dbc.Col(dcc.Loading(children=[dcc.Graph(id ='your-graph'),
-                                       dcc.Slider(id='year-slider',
-                                                 min=df['year'].min(),
-                                                 max=df['year'].max(),
-                                                 value=df['year'].min(),
-                                                 marks={str(year): str(year) for year in df['year'].unique()},
-                                                 step=None)
-                                     ], color = '#000000', type = 'dot', fullscreen=True ) ))
+app.layout = html.Div([
+    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Graph(id='graph-content')
 ])
 
-@app.callback(
- Output('your-graph', 'figure'),
- Input('year-slider', 'value'))
-def update_figure(selected_year):
- filtered_df = df[df.year == selected_year]
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_graph(value):
+    dff = df[df.country==value]
+    return px.line(dff, x='year', y='pop')
 
- fig = px.scatter(filtered_df, x="gdpPercap", y="lifeExp",
-                  size="pop", color="continent", hover_name="country",
-                  log_x=True, size_max=55)
-
- fig.update_layout(transition_duration=500)
-
- return fig
-
-if __name__=='__main__':
-    app.run_server()
-
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=5000)
