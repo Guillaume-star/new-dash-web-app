596219699f9d54730c37ec4185e82dffbae4df2a
Guillaume-star <69424028+Guillaume-star@users.noreply.github.com>
2023-11-16 20:07:01 -0500

    Update dash_flask.py

--- dash_flask.py
+++ dash_flask.py
@@ -1,7 +1,7 @@
 from dash.dependencies import Output, Input, State
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
-from dash import html
+import dash_html_components as html
 import plotly.express as px
 from flask import Flask
 import pandas as pd
